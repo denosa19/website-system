9ee44b80986977caa2aee081d7b6989616de0fa3
denosa19 <egeleon19@gmail.com>
2026-07-03 00:38:26 +0200

    yy

--- 02_components/Hero/Hero.tsx
+++ 02_components/Hero/Hero.tsx
@@ -1,11 +1,12 @@
-import { HeroProps } from "./Hero.types";
-
-export default function Hero(props: HeroProps) {
+export default function Hero({
+  title,
+  subtitle,
+}: HeroProps) {
   return (
     <section>
-      <h1>{props.title}</h1>
+      <h1>{title}</h1>
 
-      {props.subtitle && <p>{props.subtitle}</p>}
+      {subtitle && <p>{subtitle}</p>}
     </section>
   );
 }
